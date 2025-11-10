a267a761bf1fff5d3725b2eab2ee5710f66b4d38
Miguel Passos <gibertoni.passos@gmail.com>
2025-11-10 15:38:09 -0300

    evento *adicionar ao carrinho* adicionado e alguns QOL

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,14 @@
 document.addEventListener('DOMContentLoaded' , () => {
-    document.querySelectorAll('.favorite-icon').forEach(img => {
-        img.addEventListener('click', (event) => {
-            event.preventDefault();
-            if (img.src.includes('favorite-button.png')) {
-                img.src = 'assets/imgs/icons/favorite-button-hover.png'
-            } else {
-                img.src = 'assets/imgs/icons/favorite-button.png'
-            }
-        });
-    });
-    
+
     const searchInput = document.querySelector('.search');
     const galerias = document.querySelectorAll('.box-galeria');
+    const searchButton = document.querySelector('.search-button');
 
-    if (searchInput && galerias) {
-        searchInput.addEventListener('keyup', (event) => {
-            const searchTerm = event.target.value.toLowerCase();
+    searchButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (searchInput && galerias) {
+        
+            const searchTerm = searchInput.value.toLowerCase();
             galerias.forEach(galeria => {
                 const products = galeria.querySelectorAll('.box-products')
                 products.forEach(product => {
@@ -28,7 +21,37 @@ document.addEventListener('DOMContentLoaded' , () => {
                         product.style.position = 'absolute';
                     }
                 });
-            })
+            });
+        }    
+    });
+    
+    document.querySelectorAll('.favorite-icon').forEach(img => {
+        img.addEventListener('click', (event) => {
+            event.preventDefault();
+            if (img.src.includes('favorite-button.png')) {
+                img.src = 'assets/imgs/icons/favorite-button-hover.png'
+            } else {
+                img.src = 'assets/imgs/icons/favorite-button.png'
+            }
         });
-    }
+    });
+
+    const contagem = document.querySelector('.numero-produtos');
+    const comprarButton = document.querySelectorAll('.comprar-button');
+    comprarButton.forEach(img => {
+        img.addEventListener('click', (event) => {
+            event.preventDefault();
+            if (img.src.includes('comprar-button.png')) {
+                img.src = 'assets/imgs/icons/comprar-button-check.png'
+                img.classList.remove('comprar-button')
+                img.classList.add('comprar-button-2')
+            } else {
+                img.src = 'assets/imgs/icons/comprar-button.png'
+                img.classList.remove('comprar-button-2')
+                img.classList.add('comprar-button')
+            }
+        });
+    });
+    
 });
+
